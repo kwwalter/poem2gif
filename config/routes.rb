a34fa232635ea49bfa509a76eb5f03cfc3d55ed2
Kevin Walter <kevinwalter0731@gmail.com>
2015-12-07 12:31:18 -0500

    getting angular set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  root 'application#angular'
 
-  get 'welcome/index'
+  # root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
