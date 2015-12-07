b62915beac2c225dad2c5e210892133849719b70
Kevin Walter <kevinwalter0731@gmail.com>
2015-12-07 13:41:29 -0500

    getting the form set up

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -3,10 +3,32 @@ var app = angular.module('poem2GIF', ['ngRoute']);
 app.controller('MainController', ['$http', '$scope', function($http, $scope){
 
   var controller = this;
-  this.name = "kevin"; 
 
-  alert('new controller instantiated.. ', controller);
+
+
 }]);
+
+app.controller('PoemController', ['$http', '$scope', 'poemService', function($http, $scope, poemService){
+
+  var controller = this;
+
+  this.convert = function(){
+    var poem = {
+      title: controller.title,
+      body: controller.body
+    };
+    poemService.convert(poem);
+  };
+
+}]);
+
+app.service('poemService', function(){
+
+  this.convert = function(poem) {
+    // here, make the API calls
+  };
+
+});
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode({ enabled: true });
@@ -17,11 +39,11 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     controller: 'MainController',
     controllerAs: 'mainCtrl'
   }).
-  // when('/posts/:id', {
-  //   templateUrl: 'templates/posts.html',
-  //   controller: 'PostsController',
-  //   controllerAs: 'postsCtrl'
-  // }).
+  when('/new', {
+    templateUrl: 'templates/new.html.erb',
+    controller: 'PoemController',
+    controllerAs: 'poemCtrl'
+  }).
   otherwise({
     redirectTo: '/'
   });
