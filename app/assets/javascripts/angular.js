08546ab4dae62e19d2e99343ced089d333422aee
Kevin Walter <kevinwalter0731@gmail.com>
2015-12-07 12:44:13 -0500

    getting angular welcome template up and running

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -1,1 +1,28 @@
+var app = angular.module('poem2GIF', ['ngRoute']);
 
+app.controller('MainController', ['$http', '$scope', function($http, $scope){
+
+  var controller = this;
+  this.name = "kevin"; 
+
+  alert('new controller instantiated.. ', controller);
+}]);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode({ enabled: true });
+
+  $routeProvider.
+  when('/', {
+    templateUrl: 'templates/welcome.html.erb',
+    controller: 'MainController',
+    controllerAs: 'mainCtrl'
+  }).
+  // when('/posts/:id', {
+  //   templateUrl: 'templates/posts.html',
+  //   controller: 'PostsController',
+  //   controllerAs: 'postsCtrl'
+  // }).
+  otherwise({
+    redirectTo: '/'
+  });
+}]);
