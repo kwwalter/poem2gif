f338b191c2671f8ffa180a9f6294cddadf6781dd
Kevin Walter <kevinwalter0731@gmail.com>
2015-12-08 13:58:28 -0500

    did some cleanup

--- app/assets/javascripts/angular-w-service.js
+++ app/assets/javascripts/angular-w-service.js
@@ -17,10 +17,19 @@ app.controller('PoemController', ['$http', '$scope', 'poemService', function($ht
       title: controller.title,
       body: controller.body
     };
-    // controller.poemArray = poemService.convert(poem);
 
-    // poemService.convert(poem);
+    controller.poemArray = poemService.convert(poem);
 
+    console.log("at the end of poemCtrl.convert(), poemArray is now: ", controller.poemArray);
+
+    // clear the fields:
+    controller.title = "";
+    controller.body = "";
+  }; // end of convert()
+}]);
+
+app.service('poemService', ['$http', function($http){
+  this.convert = function(poem) {
     var bodystring = poem.body;
 
     // replace all the periods..
@@ -50,7 +59,7 @@ app.controller('PoemController', ['$http', '$scope', 'poemService', function($ht
         };
       }
     }
-    console.log("after the nested for loops, the linesArray is: ", linesArray);
+    // console.log("after the nested for loops, the linesArray is: ", linesArray);
 
     //here, make the API calls (using the public beta key), by iterating through the array.. using foreach instead
     linesArray.forEach(function(subarray){
@@ -83,80 +92,10 @@ app.controller('PoemController', ['$http', '$scope', 'poemService', function($ht
       }); // end of inner forEach
     }); // end of outer forEach
 
-    console.log("at the end of convert(), linesArray is now: ", linesArray);
+    console.log("at the end of poemService.convert(), linesArray is now: ", linesArray);
 
-    // clear the fields:
-    controller.title = "";
-    controller.body = "";
-  }; // end of convert()
-}]);
-
-app.service('poemService', ['$http', function($http){
-
-  // this.convert = function(poem) {
-  //   var bodystring = poem.body;
-  //
-  //   // replace all the periods..
-  //   var noPeriods = bodystring.replace(/\./g, " ");
-  //
-  //   // break into lines..
-  //   var linesArray = noPeriods.match(/[^\s.]+[^.\r\n]+[.]*/g);
-  //
-  //   for (var i = 0; i < linesArray.length; i++) {
-  //     // remove all the punctuation first..
-  //     linesArray[i] = linesArray[i].replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()&]/g,"");
-  //
-  //     // split the lines into words
-  //     linesArray[i] = linesArray[i].split(" ");
-  //   };
-  //
-  //   // console.log("linesArray is now: ", linesArray);
-  //
-  //   //here, make the API calls (using the public beta key), by iterating through the array
-  //   for (var j = 0; j < linesArray.length; j++) {
-  //     for (var k = 0; k < linesArray.length; k++) {
-  //       // set each entry up as an object
-  //       if (linesArray[j][k]) {
-  //         linesArray[j][k] = {
-  //           linkToGIF: null,
-  //           word: linesArray[j][k]
-  //         }
-  //       }
-  //
-  //       // if it's not a blank entry, make the API call
-  //       if (linesArray[j][k]) {
-  //         $http.get('http://api.giphy.com/v1/gifs/search?q=' + linesArray[j][k] + '&api_key=dc6zaTOxFJmzC ')
-  //         .then(function(data){
-  //           console.log('data from API call #j(' + j + ') #k(' + k + ') is: ', data);
-  //
-  //           // conditional to ensure that data.data.data.length > 0
-  //           if (data.data.data.length > 0) {
-  //             // generate a random index value based on data array length
-  //             var random = Math.floor(Math.random() * data.data.data.length) + 1;
-  //
-  //             // concat the gifURL using the random index
-  //             var gifURL = 'https://media.giphy.com/media/' + data.data.data[random].id + '/giphy.gif';
-  //
-  //             // if the URL is valid, assign the value.
-  //             if (gifURL) {
-  //               linesArray[j][k].linkToGIF = gifURL;
-  //             }
-  //           }
-  //         }, function(error){
-  //           console.log("error during API call: ", error);
-  //         });
-  //       } // end of if (linesArray[j][k])
-  //       console.log("j is now: ", j);
-  //       console.log("k is now: ", k);
-  //     } // end of inner for loop
-  //   } // end of outer for loop
-  //
-  //   console.log("at the end of convert(), linesArray is now: ", linesArray);
-  //
-  //   // at the end, return the array we've been working with
-  //   // return linesArray;
-  //
-  // }; // end of this.convert()
+    return linesArray;
+  }; // end of this.convert()
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
