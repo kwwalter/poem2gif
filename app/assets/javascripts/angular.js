0c05e2d9c5fbf80f704adbe53de533e87041e9ae
Kevin Walter <kevinwalter0731@gmail.com>
2015-12-21 12:29:31 -0500

    some more cleanup

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -10,16 +10,16 @@ app.controller('MainController', ['$http', '$scope', 'poemService', '$location',
 
   // get a welcome page GIF..
   $http.get('http://api.giphy.com/v1/gifs/search?q=poetry&limit=100&api_key=dc6zaTOxFJmzC ')
-  .then(function(data){
-    // console.log('data from intro API call is: ', data);
+  .then(function(res){
+    // console.log('res from intro API call is: ', res);
 
-    // conditional to ensure that data.data.data.length > 0
-    if (data.data.data.length > 0) {
+    // conditional to ensure that res.data.data.length > 0
+    if (res.data.data.length > 0) {
       // generate a random index value based on data array length.. removing the +1 to avoid the id of undefined problem
-      var random = Math.floor(Math.random() * data.data.data.length);
+      var random = Math.floor(Math.random() * res.data.data.length);
 
       // concat the gifURL using the random index
-      var gifURL = 'https://media.giphy.com/media/' + data.data.data[random].id + '/giphy.gif';
+      var gifURL = 'https://media.giphy.com/media/' + res.data.data[random].id + '/giphy.gif';
 
       // if the URL is valid, assign the value.
       if (gifURL) {
@@ -41,7 +41,7 @@ app.controller('AllController', ['$http', '$scope', 'poemService', '$location', 
 
   // tried doing this through the service, but not loading correctly.
   $http.get('/poems').then(function(res){
-    // console.log("data from get /poems call is: ", res);
+    // console.log("res from get /poems call is: ", res);
     controller.poems = res.data.poems;
   }, function(error){
     console.log("there was an error: ", error);
@@ -57,7 +57,7 @@ app.controller('ShowController', ['$http', '$scope', 'poemService', '$location',
     // controller.onePoem = poemService.getOnePoem();
     // console.log("controller.onePoem is now: ", controller.onePoem);
     $http.get('/poems/' + $routeParams.id).then(function(res){
-      // console.log("data from getOnePoem is: ", res);
+      // console.log("res from getOnePoem is: ", res);
 
       poem = res.data.poemObj;
       poem.poemContent = poemService.parsePoemArray(poem.poemContent);
@@ -91,10 +91,6 @@ app.controller('PoemController', ['$http', '$scope', 'poemService', '$location',
     };
 
     poemService.setPoemData(poem);
-
-    // controller.poemArray = poemService.convert(poem);
-
-    // poemService.convert(poem);
 
     var bodystring = poem.body;
 
@@ -135,16 +131,16 @@ app.controller('PoemController', ['$http', '$scope', 'poemService', '$location',
         // if it's not a blank entry, make the API call
         if (wordEntry) {
           $http.get('http://api.giphy.com/v1/gifs/search?q=' + wordEntry.word + '&limit=100&api_key=dc6zaTOxFJmzC ')
-          .then(function(data){
-            // console.log('data from API call when the word is ' + wordEntry.word + ' is: ', data);
+          .then(function(res){
+            // console.log('res from API call when the word is ' + wordEntry.word + ' is: ', res);
 
-            // conditional to ensure that data.data.data.length > 0
-            if (data.data.data.length > 0) {
+            // conditional to ensure that res.data.data.length > 0
+            if (res.data.data.length > 0) {
               // generate a random index value based on data array length.. removing the +1 to avoid the id of undefined problem
-              var random = Math.floor(Math.random() * data.data.data.length);
+              var random = Math.floor(Math.random() * res.data.data.length);
 
               // concat the gifURL using the random index
-              var gifURL = 'https://media.giphy.com/media/' + data.data.data[random].id + '/giphy.gif';
+              var gifURL = 'https://media.giphy.com/media/' + res.data.data[random].id + '/giphy.gif';
 
               // if the URL is valid, assign the value.
               if (gifURL) {
@@ -183,10 +179,6 @@ app.controller('ResultsController', ['$http', '$scope', 'poemService', '$locatio
   controller.poemArray = poemService.getPoemArray();
   controller.poemData = poemService.getPoemData();
 
-  // setTimeout(function(){
-  //   $('#results-buttons').append('<button ng-click="resultsCtrl.saveGIFPoem(resultsCtrl.poemArray)">YES PLEASE</button>');
-  // }, 5000);
-
   console.log("in ResultsController, poemArray is: ", controller.poemArray);
 
   // NOTE: might have to remove any blank entries (usually from periods at end of line) otherwise there'll be an error because multidimensional arrays must have subarrays of equal length
@@ -204,8 +196,8 @@ app.controller('ResultsController', ['$http', '$scope', 'poemService', '$locatio
       title: controller.poemData.title || "untitled",
       author: controller.poemData.author || "anonymous",
       poemContent: stringified
-    }).then(function(data){
-      console.log("success! data from saveGIFPoem() is: ", data);
+    }).then(function(res){
+      console.log("success! res from saveGIFPoem() is: ", res);
 
       // then redirect
       $location.path('/all')
@@ -259,71 +251,6 @@ app.service('poemService', ['$http', '$routeParams', function($http, $routeParam
     serviceThis.poemArray = null;
     serviceThis.poemData = null;
   };
-
-  // this.convert = function(poem) {
-  //   var bodystring = poem.body;
-  //
-  //   // replace all the periods..
-  //   var noPeriods = bodystring.replace(/\./g, " ");
-  //
-  //   // break into lines..
-  //   var linesArray = noPeriods.match(/[^\s.]+[^.\r\n]+[.]*/g);
-  //
-  //   for (var i = 0; i < linesArray.length; i++) {
-  //     // remove all the punctuation first..
-  //     linesArray[i] = linesArray[i].replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()&]/g,"");
-  //
-  //     // split the lines into words
-  //     linesArray[i] = linesArray[i].split(" ");
-  //   };
-  //
-  //   // console.log("linesArray is now: ", linesArray);
-  //
-  //   //here, make the API calls (using the public beta key), by iterating through the array
-  //   for (var j = 0; j < linesArray.length; j++) {
-  //     for (var k = 0; k < linesArray.length; k++) {
-  //       // set each entry up as an object
-  //       if (linesArray[j][k]) {
-  //         linesArray[j][k] = {
-  //           linkToGIF: null,
-  //           word: linesArray[j][k]
-  //         }
-  //       }
-  //
-  //       // if it's not a blank entry, make the API call
-  //       if (linesArray[j][k]) {
-  //         $http.get('http://api.giphy.com/v1/gifs/search?q=' + linesArray[j][k] + '&api_key=dc6zaTOxFJmzC ')
-  //         .then(function(data){
-  //           console.log('data from API call #j(' + j + ') #k(' + k + ') is: ', data);
-  //
-  //           // conditional to ensure that data.data.data.length > 0
-  //           if (data.data.data.length > 0) {
-  //             // generate a random index value based on data array length
-  //             var random = Math.floor(Math.random() * data.data.data.length) + 1;
-  //
-  //             // concat the gifURL using the random index
-  //             var gifURL = 'https://media.giphy.com/media/' + data.data.data[random].id + '/giphy.gif';
-  //
-  //             // if the URL is valid, assign the value.
-  //             if (gifURL) {
-  //               linesArray[j][k].linkToGIF = gifURL;
-  //             }
-  //           }
-  //         }, function(error){
-  //           console.log("error during API call: ", error);
-  //         });
-  //       } // end of if (linesArray[j][k])
-  //       console.log("j is now: ", j);
-  //       console.log("k is now: ", k);
-  //     } // end of inner for loop
-  //   } // end of outer for loop
-  //
-  //   console.log("at the end of convert(), linesArray is now: ", linesArray);
-  //
-  //   // at the end, return the array we've been working with
-  //   // return linesArray;
-  //
-  // }; // end of this.convert()
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
